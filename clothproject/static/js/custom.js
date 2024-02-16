98f4cfc01c5f894d74bf16bcc7293f1cc96e4a2d
abin <gopa7172@gmail.com>
2024-02-16 11:17:35 +0530

    add

--- clothproject/static/js/custom.js
+++ clothproject/static/js/custom.js
@@ -1,4 +1,24 @@
 function xs(){
-    let S=console.log("XS")
+    var S="XS"
+    document.getElementById("ls").innerHTML=`Size : ${S}`
+}
+function s(){
+    var S="S"
+    document.getElementById("ls").innerHTML=`Size : ${S}`
+}
+function m(){
+    var S="M"
+    document.getElementById("ls").innerHTML=`Size : ${S}`
+}
+function l(){
+    var S="L"
+    document.getElementById("ls").innerHTML=`Size : ${S}`
+}
+function xl(){
+    var S="XL"
+    document.getElementById("ls").innerHTML=`Size : ${S}`
+}
+function xxl(){
+    var S="XXL"
     document.getElementById("ls").innerHTML=`Size : ${S}`
 }
